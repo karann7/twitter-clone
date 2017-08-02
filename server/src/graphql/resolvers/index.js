10ff83816533e4d669e23ee47129936eeae4a464
Karan <kar.n@icloud.com>
2017-08-02 13:52:19 -0700

    started sorting the get all tweets by time stamp, and introduced full CRUD ability into the tweet Schema

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,7 +1,15 @@
 import TweetResolver from './tweet-resolver';
+import GraphQLDate from 'graphql-date';
 
 export default {
+  Date: GraphQLDate,
   Query: {
+    getTweet: TweetResolver.getTweet,
     getTweets: TweetResolver.getTweets,
   },
+  Mutation: {
+    createTweet: TweetResolver.createTweet,
+    updateTweet: TweetResolver.updateTweet,
+    deleteTweet: TweetResolver.deleteTweet
+  }
 };
