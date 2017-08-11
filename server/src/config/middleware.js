985300bce2abe5a162f9b1c4f15520036a22571f
Karan Singh <kar.n@icloud.com>
2017-08-10 19:57:49 -0700

    fixed the problem with auth, and not decoding should work (not tested yet..)

--- server/src/config/middleware.js
+++ server/src/config/middleware.js
@@ -4,13 +4,30 @@ import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
 import constants from './constants';
 import typeDefs from '../graphql/schema';
 import resolvers from '../graphql/resolvers';
+import { decodeToken } from '../services/auth';
 
-export default app => {
-  const schema = makeExecutableSchema({
+const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
+
+async function auth(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    if (token != null ) {
+      const user = await decodeToken(token);
+      req.user = user;
+    } else {
+      req.user = null ;
+    }
+    return next();
+  } catch (error) {
+    throw error;
+  } 
+}
+export default app => {
   app.use(bodyParser.json());
+  app.use(auth);
   app.use(
     '/graphiql',
     graphiqlExpress({
@@ -20,8 +37,11 @@ export default app => {
 
   app.use(
     constants.GRAPHQL_PATH,
-    graphqlExpress({
+    graphqlExpress(req => ({
       schema,
-    }),
+      context: {
+        user: req.user
+      }
+    })),
   );
 }
--- server/src/graphql/resolvers/tweet-resolver.js
+++ server/src/graphql/resolvers/tweet-resolver.js
@@ -3,7 +3,10 @@ import Tweet from '../../models/Tweet';
 export default {
   getTweet: (_, { _id }) => Tweet.findById(_id),
   getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
-  createTweet: (_, args) => Tweet.create(args),
+  createTweet: (_, args, ctx) => {
+    console.log(`CONTEXT ${ctx}`);
+    return Tweet.create(args)
+  },
   updateTweet: (_, { _id, ...rest }) => Tweet.findByIdAndUpdate(_id, rest, { new: true }),
   deleteTweet: async (_, { _id }) => {
     try {
